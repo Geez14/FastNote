d51b6d683a311473d14d6cf64c91e3a1a5bf53bb
Geez14 <91541128+Geez14@users.noreply.github.com>
2024-09-02 23:46:43 +0530

    update build.bat

--- build.bat
+++ build.bat
@@ -2,15 +2,17 @@
 REM compile, build
 echo "----------Compiling----------"
 javac -d out FastNoteApp.java
-if %errorlevel%  neq 0 (
+if %errorlevel% neq 0 (
   echo "Compile Failed"
+  exit /b %errorlevel%
 ) else (
   echo "Compile Successful"
-echo "-------Building FastNoteApp.jar-------"
-jar cvfm FastNoteApp.jar MANIFEST.MF -C out .
- if %errorlevel% neq 0 (
-   echo "Build Failed"
- ) else (
-  echo "Compile and Build Successful"
- )
+  echo "-------Building FastNoteApp.jar-------"
+  jar cvfm FastNoteApp.jar MANIFEST.MF -C out .
+  if %errorlevel% neq 0 (
+    echo "Build Failed"
+    exit /b %errorlevel%
+  ) else (
+    echo "Compile and Build Successful"
+  )
 )
